1bfa2775e09cf7271513feea61a92396e46b33b3
Cole Marsteller, Devin Sweeting and Michael Bradt <devin@>
2018-10-23 11:52:39 -0700

    create counting for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
 
+function countsByOne(userInputStop, userInputCount) {
+  console.log("Stop " + userInputStop);
+  console.log("Count by " + userInputCount);
+  for (var i = 0; i <= userInputStop; i += userInputCount) {
+    console.log(i);
+  };
+};
+
+
+$(document).ready(function() {
+  $("#userInput").submit(function(event) {
+     countsByOne($("input#inputCountTo").val(), parseInt($("input#inputCountBy").val()));
+
+    event.preventDefault();
+  });
 });
